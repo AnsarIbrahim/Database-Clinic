e8910ce828d106dedd91f8b6e948f66333b2509a
Supreeti Kushwaha <sksupreetk11@gmail.com>
2023-08-22 18:28:34 +0530

    add key_constraints for tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -61,3 +61,38 @@ CREATE TABLE "treatments"(
     "type" VARCHAR(255) NOT NULL,
     "name" VARCHAR(255) NOT NULL
 );
+
+ALTER TABLE
+    "treatments"
+ADD
+    PRIMARY KEY("id");
+
+ALTER TABLE
+    "invoice_items"
+ADD
+    CONSTRAINT "invoice_items_invoice_id_foreign" FOREIGN KEY("invoice_id") REFERENCES "invoices"("id");
+
+ALTER TABLE
+    "treatments"
+ADD
+    CONSTRAINT "treatments_id_foreign" FOREIGN KEY("id") REFERENCES "medical_histories"("id");
+
+ALTER TABLE
+    "medical_histories"
+ADD
+    CONSTRAINT "medical_histories_id_foreign" FOREIGN KEY("id") REFERENCES "treatments"("id");
+
+ALTER TABLE
+    "invoices"
+ADD
+    CONSTRAINT "invoices_medical_history_id_foreign" FOREIGN KEY("medical_history_id") REFERENCES "medical_histories"("id");
+
+ALTER TABLE
+    "medical_histories"
+ADD
+    CONSTRAINT "medical_histories_patient_id_foreign" FOREIGN KEY("patient_id") REFERENCES "patients"("id");
+
+ALTER TABLE
+    "invoice_items"
+ADD
+    CONSTRAINT "invoice_items_treatment_id_foreign" FOREIGN KEY("treatment_id") REFERENCES "treatments"("id");
